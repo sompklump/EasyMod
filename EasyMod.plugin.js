09145ec4a465dabdef708421ce0fdb9d71eb238e
Klump <23707746+sompklump@users.noreply.github.com>
2022-01-08 13:50:48 +0100

    Add files via upload

--- EasyMod.plugin.js
+++ EasyMod.plugin.js
@@ -1,10 +1,12 @@
 /**
  * @name EasyMod
- * @version 0.0.1
+ * @version 0.0.2
  * @description Easier way to moderate you community
  * @author Klump
  * @authorId 164191150540980224
  * @invite 5AZmY4V6AU
+ * @source https://github.com/sompklump/EasyMod/blob/master/EasyMod.plugin.js
+ * @updateUrl https://raw.githubusercontent.com/sompklump/EasyMod/master/EasyMod.plugin.js
  *
  */
 
