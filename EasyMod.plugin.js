cb2da21da060bd1f1055554be05fe596338a7326
Klump <23707746+sompklump@users.noreply.github.com>
2022-01-08 13:47:57 +0100

    Add files via upload

---
+++ EasyMod.plugin.js
@@ -0,0 +1,26 @@
+/**
+ * @name EasyMod
+ * @version 0.0.1
+ * @description Easier way to moderate you community
+ * @author Klump
+ * @authorId 164191150540980224
+ * @invite 5AZmY4V6AU
+ *
+ */
+
+
+
+module.exports = class Example {
+
+    load() {}
+    start() {
+
+        BdApi.alert("Yooo");
+        //code here
+
+        //stop coding
+    }
+    stop() {
+        BdApi.alert(":(");
+    }
+}
